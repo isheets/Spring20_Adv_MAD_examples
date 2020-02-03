94c3d36b4c626c85c8b116d76483c07481367b53
Isaac Sheeets <icsheets97@gmail.com>
2020-02-02 21:28:32 -0700

    added reordering ability to country list

--- week-04-navigation-controllers/countries/countries/DetailViewController.swift
+++ week-04-navigation-controllers/countries/countries/DetailViewController.swift
@@ -24,8 +24,7 @@ class DetailViewController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -50,15 +49,15 @@ class DetailViewController: UITableViewController {
         return cell
     }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -68,22 +67,34 @@ class DetailViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        //get idx of rows
+        let fromRow = fromIndexPath.row
+        let toRow = to.row
+        
+        //get name of country being moved
+        let moveCountry = countryList[fromRow]
+        
+        //swap in array
+        countryList.swapAt(fromRow, toRow)
+        
+        //swap in data controller
+        continentsData.deleteCountry(dataIdx: selectedContinent, countryIdx: fromRow)
+        continentsData.addCountry(dataIdx: selectedContinent, newCountry: moveCountry, countryIdx: toRow)
     }
-    */
+    
 
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+    
 
     /*
     // MARK: - Navigation
