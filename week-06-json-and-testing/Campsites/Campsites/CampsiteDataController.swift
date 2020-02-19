030d04c40542df979d935195f3638342107e2639
Isaac Sheeets <icsheets97@gmail.com>
2020-02-19 15:17:29 -0700

    completed demo app

--- week-06-json-and-testing/Campsites/Campsites/CampsiteDataController.swift
+++ week-06-json-and-testing/Campsites/Campsites/CampsiteDataController.swift
@@ -15,38 +15,70 @@ enum JsonError: Error {
 }
 
 class CampsiteDataController {
+    //stores all of the campsites from the most recent response
+    var currentCampsites = CampsiteData()
     
-    func loadJson(stateCode: String) throws -> [Campsite] {
+    var onDataUpdate: ((_ data: [Campsite]) -> Void)?
+    
+    //makes the http request based on stateCode parameter
+    func loadJson(stateCode: String) throws {
         //construct URL by interpolating the state code into
         let urlPath = "https://developer.nps.gov/api/v1/campgrounds?stateCode=\(stateCode)&api_key=KuzYWrSKHbuz6CBO8oc0pX35CeljxNSfgxane4IH"
         
         //use a guard statement with conditional unwrapping to make sure the url is valid
         guard let url = URL(string: urlPath) else {
-            print("url error")
             throw JsonError.BadURL
         }
+        let group = DispatchGroup()
+        group.enter()
         
+        
+        //valid url so make the request and give it a completetion handler closure
         let session = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+            //downcase to URLResponse since we made and https request
             let httpResponse = response as! HTTPURLResponse
+            
+            //get the status code
             let statusCode = httpResponse.statusCode
+            
+            //make sure we got a good response
             guard statusCode == 200 else {
-                    print("file download error")
-                    return
+                print("file download error")
+                return
             }
             //download successful
             print("download complete")
             //parse json
-            parseJson(rawData: data!)
+            DispatchQueue.main.async {self.parseJson(rawData: data!)}
         })
-        //must call resume to run session
+        
+        //must call resume to run session and execute request
         session.resume()
     }
     
-    func parseJson(rawData: Data) -> [Campsite] {
+    //parses the raw http response and notifies the view controller
+    func parseJson(rawData: Data)  {
+        do {
+            //try to decode the response
+            let parsedData = try JSONDecoder().decode(CampsiteData.self, from: rawData)
+            //clear out old data
+            currentCampsites.data.removeAll()
+            //add all the campsite entries to our class property that stores the current campsites
+            for campsite in parsedData.data {
+                currentCampsites.data.append(campsite)
+            }
+        } catch {
+            //something went wrong parsing the data — throw error!
+            print("json error")
+            print(error.localizedDescription)
+        }
+        print("parsejson done")
         
+        onDataUpdate?(currentCampsites.data)
     }
     
-    func getResults() {
-        
+    //returns the current data
+    func getResults() -> [Campsite] {
+        return currentCampsites.data
     }
 }
---
+++ week-06-json-and-testing/Campsites/Campsites/ResultsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ResultsViewController.swift
+//  Campsites
+//
+//  Created by Isaac Sheets on 2/19/20.
+//  Copyright © 2020 Isaac Sheets. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UITableViewController {
+
+    var results = [Campsite]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return results.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CampsiteCell", for: indexPath)
+        
+        //set the title of cell label
+        cell.textLabel!.text = results[indexPath.row].name
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailSegue" {
+            //get the selected campsite
+            let idx = tableView.indexPath(for: sender as! UITableViewCell)!.row
+            let selectedCampsite = results[idx]
+            //get reference to detail vc
+            let detailVC = segue.destination as! DetailViewController
+            
+            //set detail vc properties
+            detailVC.title = selectedCampsite.name
+            detailVC.siteDirections = selectedCampsite.directionsoverview
+            detailVC.siteDescription = selectedCampsite.description
+        }
+    }
+
+}
