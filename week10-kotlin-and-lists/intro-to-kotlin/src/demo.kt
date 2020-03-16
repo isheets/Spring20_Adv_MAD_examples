132dbeb03fc1a80f9ba9104ec01e630ce6a47a9f
Isaac Sheeets <icsheets97@gmail.com>
2020-03-16 14:34:20 -0600

    working on demo

--- week10-kotlin-and-lists/intro-to-kotlin/src/demo.kt
+++ week10-kotlin-and-lists/intro-to-kotlin/src/demo.kt
@@ -2,9 +2,10 @@ import kotlin.reflect.typeOf
 
 //the main function is the entry point to our program
 fun main() {
-    //region: constants, variables, and types
+    /*
+    //region: CONSTANTS, VARIABLES, AND TYPES
 
-    //region: var and val
+    //region: VAR AND VAL
     //variables (implicit type of Int)
     var myVar = 2 //implicit type (int)
     println(myVar)
@@ -20,7 +21,7 @@ fun main() {
     //endregion
 
 
-    //region: basic types (explicitly typed)
+    //region: BASIC TYPES (explicitly typed)
 
     //Integers
     val myInt: Int = 4
@@ -75,21 +76,24 @@ fun main() {
 
     //endregion
 
-    //region: nullable types and checking
+    //region: NULLABLE TYPES AND CHECKING
     //must have explicit type with question mark to hold a null value
     var score: Int? = null
 
     //null check (fails)
     if (score != null) {
-        println("Score: $score")
+        println("Score: ${score.minus(10)}")
     } else {
         println("score is null")
     }
 
+    //give score a value
     score = 80
 
+    //null check (succeeds)
     if (score != null) {
-        println("Score: $score")
+        //compiler remembers that we checked null so we can access methods/properties of score inside this block
+        println("Score: ${score.minus(10)}")
     }
 
     score = null
@@ -102,10 +106,108 @@ fun main() {
     println("Elvis Operator:")
     println(score?.minus(10) ?: 10)
 
-    //!! operator -- force not null assertion
-    
+    //!! operator -- force not null assertion -- BAD
+    //println("Score: ${score!!.toString()}") //causes Runtime Error - Null Pointer Exception :(
+    //endregion
 
     //endregion
+
+    //region: TYPE CHECKS AND CASTING
+
+    //is keyword -- checks type and automatically casts in most cases
+    var mixedArray = arrayOf("Test string", 'C', 1_000, 1.00)
+
+    fun typeChecker(x: Any) {
+        if(x is String) {
+            println("String has length ${x.length}") //x is automatically cast to string
+        }
+        else {
+            println("$x is not a string")
+        }
+    }
+
+    //check types of each element in array
+    mixedArray.map{typeChecker(it)}
+
+    //as (unsafe) and as? (safe) keyword to cast to specific type
+    for(item in mixedArray) {
+        var cast = item as? String //change to as and see what happens
+        if(cast != null) {
+            println("successfully cast $cast as String")
+        }
+    }
+
+    //endregion
+
+    //region: LOOPS and EXPRESSIONS
+
+    //RANGES
+    //Kotlin uses ".." syntax and
+    for(i in 1..5) println(i)
+    println()
+    for(i in 1 until 5) println(i)
+    println()
+    for(i in 1..5 step 2) println(i)
+    println()
+    for(i in 5 downTo 1) println(i)
+    println()
+
+
+    //WHEN EXPRESSIONS
+    val myScore = 80
+    val highScore = 120
+
+    //replaces a C style switch statement
+    when(myScore) {
+        in 0 until highScore -> println("current score less than high score")
+        highScore -> println("new high score!")
+        else -> println("above high score")
+    }
+    println()
+
+    //FOR LOOPS
+    val fruits = arrayOf("Apples", "Bananas", "Strawberries", "Cherries", "Raspberries")
+
+    //basic syntax
+    for(fruit in fruits) {
+        println("Current fruit: $fruit")
+    }
+    println()
+
+    //get the index of the iterator
+    for((i, fruit) in fruits.withIndex()) {
+        println("Fruit #$i: $fruit")
+    }
+
+    println()
+
+    //use indices
+    for(i in fruits.indices) {
+        println("Fruit #$i: ${fruits[i]}")
+    }
+    println()
+
+    //WHILE LOOPS
+    //nothing new here
+    var loop = 5
+    while(loop > 0) {
+        println(loop)
+        loop--
+    }
+
+    //endregion
+    */
+
+    //region: COLLECTIONS
+
+    //LIST
+    val myList = listOf(1,2,3,4,5,6)
+    //myList[0] = 10 //won't compile since this list is read-only
+
+    val myMutableList = mutableListOf(1,2,3,4,5,6)
+    myMutableList[0] = 10
+
+
 
     //endregion
 }
