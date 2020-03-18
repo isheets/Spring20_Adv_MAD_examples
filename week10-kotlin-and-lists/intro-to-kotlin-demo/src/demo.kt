a803b66d3f7add67cba0b92b4ace0ef4e28f79b2
Isaac Sheets <isaac@Isaacs-MacBook-Pro.local>
2020-03-18 09:51:47 -0600

    second and third videos

--- week10-kotlin-and-lists/intro-to-kotlin-demo/src/demo.kt
+++ week10-kotlin-and-lists/intro-to-kotlin-demo/src/demo.kt
@@ -79,8 +79,80 @@ fun main() {
     println(userName!!.length)
 
     //endregion
+
+    //region: TYPE CHECKS and CASTING
+
+    //"is" keyword
+    var mixedArray = arrayOf("Test string", 'C', 100, 1.5)
+
+    fun stringChecker(x: Any) {
+        if(x is String) {
+            println("$x has length ${x.length}")
+        } else {
+            println("$x is not a string")
+        }
+    }
+
+    mixedArray.map{stringChecker(it)}
+
+    //"as" keyword
+    for(item in mixedArray) {
+        var cast = item as? String
+        println(cast ?: "not a string")
+    }
+
+    //endregion
+
     */
 
-    //region: 
+
+    //region: LOOPS and CONTROLS
+    //RANGES
+    for(i in 1..5) print("$i ")
+    println()
+    for(i in 1 until 5) print("$i ")
+    println()
+    for(i in 1..5 step 2) print("$i ")
+    println()
+    for(i in 5 downTo 1) print("$i ")
+    println()
+    println()
+
+    //WHEN EXPRESSION
+    val myScore = 110
+    val highScore = 100
+
+    when(myScore) {
+        in 0 until highScore -> println("below high score")
+        highScore -> println("at high score")
+        else -> println("new high score!")
+    }
+
+    println()
+
+    //FOR LOOPS
+    val fruits = arrayOf("Apples", "Oranges", "Bananas", "Cherries")
+
+    for(fruit in fruits) {
+        println("Current fruit: $fruit")
+    }
+
+    for((i, fruit) in fruits.withIndex()) {
+        println("Fruit #$i: $fruit")
+    }
+
+    for(i in fruits.indices) {
+        println("Fruit #$i: ${fruits[i]}")
+    }
+
+    //WHILE
+    var loop = 5
+
+    while (loop > 0) {
+        println(loop)
+        loop--
+    }
+    
+    //endregion
 
 }
