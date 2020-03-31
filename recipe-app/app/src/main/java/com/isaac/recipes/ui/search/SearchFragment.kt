ee6aac1b36d70fd1693c507e6e11c98739e7daf7
Isaac Sheets <isaac@Isaacs-MacBook-Pro.local>
2020-03-30 22:05:23 -0600

    finished recyclerview implementation

--- recipe-app/app/src/main/java/com/isaac/recipes/ui/search/SearchFragment.kt
+++ recipe-app/app/src/main/java/com/isaac/recipes/ui/search/SearchFragment.kt
@@ -9,11 +9,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.isaac.recipes.R
 
 class SearchFragment : Fragment() {
 
     private lateinit var searchViewModel: SearchViewModel
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -23,19 +25,17 @@ class SearchFragment : Fragment() {
         //instance of ViewModel
         searchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
 
+        val root = inflater.inflate(R.layout.fragment_search, container, false)
+        recyclerView = root.findViewById(R.id.recyclerView)
+
         //subscribe to data changes in the repository class via the ViewModel
         searchViewModel.recipeData.observe(viewLifecycleOwner, Observer {
-            for(recipe in it) {
-                Log.i("recipeLogging", "${recipe.title} serves ${recipe.servings}")
-            }
-
+            //instantiate adapter
+            val adapter = SearchRecyclerAdapter(requireContext(), it)
+            //set the adapter to the recyclerview
+            recyclerView.adapter = adapter
         })
 
-        val root = inflater.inflate(R.layout.fragment_search, container, false)
-        val textView: TextView = root.findViewById(R.id.text_search)
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
---
+++ recipe-app/app/src/main/java/com/isaac/recipes/ui/search/SearchRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.isaac.recipes.ui.search
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.isaac.recipes.R
+import com.isaac.recipes.data.Recipe
+
+class SearchRecyclerAdapter(val context: Context, val recipeList: List<Recipe>) : RecyclerView.Adapter<SearchRecyclerAdapter.ViewHolder>() {
+
+    //custom ViewHolder
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleText = itemView.findViewById<TextView>(R.id.titleTextView)
+        val prepText = itemView.findViewById<TextView>(R.id.prepTextView)
+    }
+
+    //inflate the view for the item
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recipe_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    //number of items in RecyclerView
+    override fun getItemCount() = recipeList.count()
+
+    //set the data for the view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val curRecipe = recipeList[position]
+
+        holder.titleText.text = curRecipe.title
+        holder.prepText.text = "${curRecipe.readyInMinutes} min"
+    }
+}
