7a37a8ca175186e270624f98874d4d9573c877a6
Isaac Sheets <isaac@Isaacs-MacBook-Pro.local>
2020-04-05 11:44:50 -0600

    added missing dependencies and a place for global constants

---
+++ recipe-app/app/src/main/java/com/isaac/recipes/Global.kt
@@ -0,0 +1,9 @@
+package com.isaac.recipes
+
+//constants for API request
+const val BASE_URL = "https://api.spoonacular.com"
+const val API_KEY = "c70a9dfbf44e4cefb9074f0a7e897fa8"
+const val IMAGE_BASE_URL = "https://spoonacular.com/recipeImages"
+
+//general constants
+const val LOG_TAG = "recipeLogging"
