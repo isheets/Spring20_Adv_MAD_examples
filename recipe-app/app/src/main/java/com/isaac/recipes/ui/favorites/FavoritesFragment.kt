295540029ff022e679b15982b9d4025e6accb5be
Isaac Sheets <isaac@Isaacs-MacBook-Pro.local>
2020-04-12 11:55:25 -0600

    end product of last demo

--- recipe-app/app/src/main/java/com/isaac/recipes/ui/favorites/FavoritesFragment.kt
+++ recipe-app/app/src/main/java/com/isaac/recipes/ui/favorites/FavoritesFragment.kt
@@ -1,6 +1,7 @@
 package com.isaac.recipes.ui.favorites
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.isaac.recipes.LOG_TAG
 import com.isaac.recipes.R
 
 class FavoritesFragment : Fragment() {
@@ -22,6 +24,10 @@ class FavoritesFragment : Fragment() {
                 ViewModelProvider(this).get(FavoritesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_favorites, container, false)
         val textView: TextView = root.findViewById(R.id.text_favorites)
+
+        favoritesViewModel.favoriteWithDetailsList.observe(viewLifecycleOwner, Observer {
+            Log.i(LOG_TAG, it.toString())
+        })
 
         //listen for updates to our LiveData object
         favoritesViewModel.text.observe(viewLifecycleOwner, Observer {
--- recipe-app/app/src/main/java/com/isaac/recipes/ui/favorites/FavoritesViewModel.kt
+++ recipe-app/app/src/main/java/com/isaac/recipes/ui/favorites/FavoritesViewModel.kt
@@ -1,10 +1,15 @@
 package com.isaac.recipes.ui.favorites
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.isaac.recipes.data.FavoriteRepository
 
-class FavoritesViewModel : ViewModel() {
+class FavoritesViewModel(app: Application) : AndroidViewModel(app) {
+    private val favRepo = FavoriteRepository(app)
+    val favoriteWithDetailsList = favRepo.allFavoritesWithDetails
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is the Favorites Fragment"
