a4830b977b75ce65d6f54f2d284ab1b55d4fbebe
Isaac Sheets <isaac@Isaacs-MacBook-Pro.local>
2020-04-22 20:54:52 -0600

    read and add favorites to firebase

--- recipe-app/app/src/main/java/com/isaac/recipes/data/repos/FavoriteRepository.kt
+++ recipe-app/app/src/main/java/com/isaac/recipes/data/repos/FavoriteRepository.kt
@@ -1,76 +1,171 @@
 package com.isaac.recipes.data.repos
 
 import android.app.Application
-import android.database.Observable
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.isaac.recipes.data.database.AppDatabase
-import com.isaac.recipes.data.database.favorite.Favorite
-import com.isaac.recipes.data.database.ingredient.Ingredient
-import com.isaac.recipes.data.database.instruction.Instruction
-import com.isaac.recipes.data.database.relation.FavoriteWithDetails
-import com.isaac.recipes.data.models.Recipe
-import com.isaac.recipes.data.models.RecipeDetails
-import java.util.*
-import androidx.lifecycle.Observer
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.isaac.recipes.LOG_TAG
-import kotlinx.coroutines.*
+import com.isaac.recipes.data.models.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class FavoriteRepository(val app: Application) {
-    private val db = AppDatabase.getDatabase(app)
+    private val db = Firebase.firestore
 
-    //dao references
-    private val favoriteWithDetailsDAO = db.favoriteWithDetailsDAO()
-    private val favoriteDAO = db.favoriteDAO()
-    private val ingredientDAO = db.ingredientDAO()
-    private val instructionDAO = db.instructionDAO()
-
-    val favoriteRoomList: LiveData<List<Favorite>> = favoriteDAO.getAllFavorites()
-
-
-    //transforms RecipeWithDetails to Favorite, List<Instructions>, List<Ingredients> and adds to Room using DAOs
-    fun addFavorite(recipe: RecipeDetails) {
-        CoroutineScope(Dispatchers.IO).launch {
-            //insert the favorite
-            favoriteDAO.insertFavorite(recipe.getRoomFavorite())
-            //insert each instruction
-            for(instruction in recipe.getRoomInstructions()) {
-                instructionDAO.insertInstruction(instruction)
-            }
-            //insert each ingredient
-            for(ingredient in recipe.getRoomIngredients()) {
-                ingredientDAO.insertIngredient(ingredient)
-            }
-        }
-    }
-
-    val favoriteDetails: MutableLiveData<RecipeDetails> = MutableLiveData()
-
-    fun getDetailsForRecipe(recipe: Recipe) {
-        CoroutineScope((Dispatchers.IO)).launch {
-            val fav = favoriteDAO.getFavorite(recipe.id)
-            val instructions = instructionDAO.getInstructionsForRecipe(fav.recipe_id)
-            val ingredients = ingredientDAO.getIngredientsForRecipe(fav.recipe_id)
-
-            favoriteDetails.postValue(RecipeDetails.fromRoomTypes(fav, instructions, ingredients))
-        }
-    }
-
-    fun removeRecipeFromFavorites(id: Int) {
-        CoroutineScope(Dispatchers.IO).async {
-            ingredientDAO.deleteIngredients(id)
-            instructionDAO.deleteInstructions(id)
-            favoriteDAO.removeFavorite(id)
-        }
-    }
+    val favoriteList = MutableLiveData<List<Recipe>>()
+    val favoriteDetails = MutableLiveData<RecipeDetails>()
+    private val allData: MutableMap<Int, RecipeDetails> = mutableMapOf()
 
     val recipeIsFavorite = MutableLiveData<Boolean>()
 
-    fun isRecipeFavorited(id: Int) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val favorite = favoriteDAO.getFavorite(id)
-            recipeIsFavorite.postValue( favorite != null)
-        }
+    init {
+        db.collection("favorites")
+            .addSnapshotListener { snapshot, e ->
+                if(e != null) {
+                    Log.e(LOG_TAG, "Listen failed.", e)
+                    return@addSnapshotListener
+                }
+                if (snapshot != null) {
+                    parseAllData(snapshot)
+                } else {
+                    Log.d(LOG_TAG, "Current data: null")
+                }
+
+            }
+
     }
+
+    private fun parseAllData(result: QuerySnapshot) {
+        val allFavorites = mutableListOf<Recipe>()
+        for(doc in result) {
+            //get the data from the document
+            val id: Int = (doc.get("id") as Long).toInt()
+            val title: String = doc.getString("title")!!
+            val summary: String = doc.getString("summary")!!
+            val image: String = doc.getString("image")!!
+            val readyInMinutes: Int = (doc.get("readyInMinutes") as String).toInt()
+            val servings: Int = doc.getString("servings")!!.toInt()
+            val genericInstructions: ArrayList<*> = doc.get("instructions") as ArrayList<*>
+            val genericIngredients : ArrayList<*> = doc.get("ingredients") as ArrayList<*>
+
+            //add to all favorites array
+            allFavorites.add(Recipe(
+                id,
+                title,
+                readyInMinutes,
+                servings,
+                image
+            ))
+
+            allData[id] = RecipeDetails(
+                id,
+                title,
+                summary,
+                image,
+                readyInMinutes,
+                servings,
+                listOf(Steps(getRecipeInstructions(genericInstructions))),
+                getRecipeIngredients(genericIngredients)
+            )
+        }
+
+        Log.i(LOG_TAG, "allData: $allData")
+
+        favoriteList.value = allFavorites
+    }
+
+    private fun getRecipeInstructions(dbArray: ArrayList<*>): List<Instruction> {
+        val instructions = mutableListOf<Instruction>()
+        for(obj in dbArray) {
+            val map = obj as HashMap<*, *>
+            instructions.add(
+                Instruction(
+                    (map["number"] as String).toInt(),
+                    map["step"] as String
+                ))
+        }
+
+        return instructions
+    }
+
+    private fun getRecipeIngredients(dbArray: ArrayList<*>): Set<Ingredient> {
+        val ingredients = mutableSetOf<Ingredient>()
+        for(obj in dbArray) {
+            val map = obj as HashMap<*, *>
+            ingredients.add(Ingredient(map["originalString"] as String,
+                map["name"] as String,
+                (map["amount"] as String).toDouble(),
+                map["unit"] as String
+            ))
+        }
+        return ingredients
+    }
+
+    fun isRecipeFavorited(id: Int) {
+        recipeIsFavorite.value = allData.containsKey(id)
+    }
+
+
+
+    fun getDetailsForRecipe(recipe: Recipe) {
+        favoriteDetails.value = allData[recipe.id]
+    }
+
+    fun removeRecipeFromFavorites(id: Int) {}
+
+    fun addFavorite(recipe: RecipeDetails) {
+        val recipeMap = recipeDetailsToHashMap(recipe)
+
+        db.collection("favorites").add(recipeMap)
+            .addOnSuccessListener {
+                Log.i(LOG_TAG, "Added favorite success!")
+            }
+            .addOnFailureListener { exception ->
+                Log.w(LOG_TAG, "Error adding document.", exception)
+            }
+    }
+
+    private fun recipeDetailsToHashMap(recipe: RecipeDetails): HashMap<String, *> {
+        val map = hashMapOf(
+            "id" to recipe.id,
+            "title" to recipe.title,
+            "summary" to recipe.summary,
+            "image" to recipe.image,
+            "readyInMinutes" to recipe.readyInMinutes.toString(),
+            "servings" to recipe.servings.toString(),
+            "ingredients" to ingredientsToArrayOfMaps(recipe.extendedIngredients),
+            "instructions" to instructionsToArrayOfMaps(recipe.analyzedInstructions[0].steps)
+        )
+        return map
+    }
+
+    private fun ingredientsToArrayOfMaps(ingredients: Set<Ingredient>): ArrayList<HashMap<String, *>> {
+        val ingredientArrayList = ArrayList<HashMap<String, *>>()
+
+        for(ingredient in ingredients) {
+            ingredientArrayList.add(hashMapOf(
+                "originalString" to ingredient.originalString,
+                "name" to ingredient.name,
+                "amount" to ingredient.amount.toString(),
+                "unit" to ingredient.unit
+            ))
+        }
+        return ingredientArrayList
+    }
+
+    private fun instructionsToArrayOfMaps(instructions: List<Instruction>): ArrayList<HashMap<String, *>> {
+        val instructionArrayList = ArrayList<HashMap<String, *>>()
+
+        for(instruction in instructions) {
+            instructionArrayList.add(hashMapOf(
+                "number" to instruction.number.toString(),
+                "step" to instruction.step
+            ))
+        }
+
+        return instructionArrayList
+    }
+
 }
--- recipe-app/app/src/main/java/com/isaac/recipes/ui/favorites/SharedFavoritesViewModel.kt
+++ recipe-app/app/src/main/java/com/isaac/recipes/ui/favorites/SharedFavoritesViewModel.kt
@@ -12,29 +12,9 @@ class SharedFavoritesViewModel(app: Application) : AndroidViewModel(app) {
 
     val favDetails: MutableLiveData<RecipeDetails> = favRepo.favoriteDetails
 
-    val favoriteRecipeList: MutableLiveData<List<Recipe>> = MutableLiveData()
+    val favoriteRecipeList: MutableLiveData<List<Recipe>> = favRepo.favoriteList
 
     var recipeToShow = 0
-
-    private val favoriteListObserver =  Observer<List<Favorite>> {
-        val allRecipes = mutableListOf<Recipe>()
-
-        for(fav in it) {
-            allRecipes.add(Recipe.fromRoomFavorite(fav))
-        }
-
-        favoriteRecipeList.value = allRecipes
-    }
-
-
-    init {
-        favRepo.favoriteRoomList.observeForever(favoriteListObserver)
-    }
-
-    override fun onCleared() {
-        favRepo.favoriteRoomList.removeObserver(favoriteListObserver)
-        super.onCleared()
-    }
 
     //get selected favorites details and update live data
     fun favSelected(recipe: Recipe) {
